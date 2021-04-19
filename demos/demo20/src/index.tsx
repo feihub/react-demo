1b1cf1f422bebd101f562863a3a55e49ba96bd71
wangxiaofei <wangxiaofei2004@163.com>
2021-04-19 22:30:33 +0800

    #7 综合项目-- 1 搭建项目 -- 注释 react router 2

--- demos/demo20/src/index.tsx
+++ demos/demo20/src/index.tsx
@@ -6,16 +6,35 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import { Counter } from './features/counter/Counter';
-import { BrowserRouter, HashRouter, Route } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     {/* 2.配置redux store给React使用 */}
     <Provider store={store}>
-      <HashRouter>
-        <Route path="/home" component={App}></Route>
-        <Route path="/clock" component={Counter}></Route>
-      </HashRouter>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/clock">clock</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Switch>
+            <Route exact path="/">
+              <App />
+            </Route>
+            <Route path="/clock">
+              <Counter />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
