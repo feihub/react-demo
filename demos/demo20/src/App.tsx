0e9829a3af56ac39b873cb22ebb01f197af969da
wangxiaofei <wangxiaofei2004@163.com>
2021-04-19 22:17:01 +0800

    综合项目-- 1 搭建项目 -- 注释 react router

--- demos/demo20/src/App.tsx
+++ demos/demo20/src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import './App.css';
 import './i18n'
 // the hook
 import { useTranslation } from 'react-i18next';
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { Counter } from './features/counter/Counter';
+import { Layout, Menu, Breadcrumb, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { MenuInfo } from 'rc-menu/lib/interface';
 
 const { Header, Content, Footer } = Layout;
 
@@ -12,15 +12,40 @@ function App() {
 
   const { t, i18n } = useTranslation();
 
+  const menu = (
+    <Menu onClick={(e) => handleMenuClick(e)}>
+      <Menu.Item key="1">
+        English
+      </Menu.Item>
+      <Menu.Item key="2">
+        中文
+      </Menu.Item>
+    </Menu>
+  );
+
+  function handleMenuClick(e: MenuInfo) {
+    if (e.key == '1') {
+      i18n.changeLanguage('en');
+    } else {
+      i18n.changeLanguage('zh');
+    }
+
+  }
+
   return (
     <div className="App">
       <Layout className="layout">
         <Header>
           <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
             <Menu.Item key="1">{t('nav1')}</Menu.Item>
             <Menu.Item key="2">{t('nav2')}</Menu.Item>
             <Menu.Item key="3">{t('nav3')}</Menu.Item>
+            <Dropdown overlay={menu}>
+                <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                  {t('Language')} <DownOutlined />
+                </a>
+            </Dropdown>
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px' }}>
@@ -29,7 +54,7 @@ function App() {
             <Breadcrumb.Item>{t('List')}</Breadcrumb.Item>
             <Breadcrumb.Item>{t('App')}</Breadcrumb.Item>
           </Breadcrumb>
-          <div className="site-layout-content">{t('Content')}<Counter/></div>
+          <div className="site-layout-content">{t('Content')}</div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>{t('Ant Design ©2018 Created by Ant UED')}</Footer>
       </Layout>
--- demos/demo20/src/index.tsx
+++ demos/demo20/src/index.tsx
@@ -5,12 +5,17 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { Counter } from './features/counter/Counter';
+import { BrowserRouter, HashRouter, Route } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     {/* 2.配置redux store给React使用 */}
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <Route path="/home" component={App}></Route>
+        <Route path="/clock" component={Counter}></Route>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
