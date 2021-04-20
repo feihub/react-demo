d0d2c23f11335a495647a743d346b7fb55aecb0a
wangxiaofei <wangxiaofei2004@163.com>
2021-04-20 16:48:25 +0800

    Update App.test.js

--- demos/demo13/src/App.test.js
+++ demos/demo13/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//https://jestjs.io/docs/getting-started
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
