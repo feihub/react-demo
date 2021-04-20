f4c8f94ef07b0f07239e502f01c35e9e1057fe4c
wangxiaofei <wangxiaofei2004@163.com>
2021-04-20 14:58:50 +0800

    Update store.js

--- demos/demo16/src/app/store.js
+++ demos/demo16/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
+//1.创建并导出Redux store
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
