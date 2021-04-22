e18eb831b9f48c0c8e97f6b48cd122dddb1ce1e4
wangxiaofei <wangxiaofei2004@163.com>
2021-04-22 16:10:20 +0800

    Update Login.tsx

--- demos/demo20/src/features/login/Login.tsx
+++ demos/demo20/src/features/login/Login.tsx
@@ -14,42 +14,11 @@ return (
 
       {if(!login){
       
-      <Form>
-        <Form.Group controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="name" placeholder="Enter name" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-                  
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+            <LoginFrom/>
       
-}else {
+      }else {
       
-      <Form>
-        <Form.Group controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="name" placeholder="Name" plaintext readOnly />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-       
-      }
+            <LogoutFrom/>
       }
 );
 
