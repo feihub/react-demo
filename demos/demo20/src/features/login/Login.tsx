9de9ce8e2a1a2029d47759dc8180558bf311a491
wangxiaofei <wangxiaofei2004@163.com>
2021-04-22 13:33:20 +0800

    Create Login.tsx

---
+++ demos/demo20/src/features/login/Login.tsx
@@ -0,0 +1,34 @@
+import { Alert } from 'react-bootstrap';
+// the hook
+import { useTranslation } from 'react-i18next';
+
+export function Login() {
+
+const { t, i18n } = useTranslation();
+
+return (
+
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+);
+
+}
