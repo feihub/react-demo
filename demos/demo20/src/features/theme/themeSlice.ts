88cb65a1accaf0d783df575208b86c0cb9c12a4b
wangxiaofei <wangxiaofei2004@163.com>
2021-04-22 14:30:34 +0800

    Update and rename demos/demo20/src/features/themeSlice.ts to demos/demo20/src/features/theme/themeSlice.ts

---
+++ demos/demo20/src/features/theme/themeSlice.ts
@@ -0,0 +1,30 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+
+export interface ThemeState {
+  theme: string;
+}
+
+const initialState: ThemeState = {
+  theme: 'light'
+};
+
+//3.创建state slice用于生成action和reducer
+export const counterSlice = createSlice({
+  name: 'theme',
+  initialState,
+  reducers: {
+    changeThemeLight: (state) => {
+      state.theme = 'light';
+    },
+    changeThemeDark: (state) => {
+      state.theme = 'dark';
+    }
+  }
+});
+
+export const { changeThemeLight, changeThemeDark } = themeSlice.actions;
+
+export const selectTheme = (state: RootState) => state.theme.theme;
+
+export default themeSlice.reducer;
