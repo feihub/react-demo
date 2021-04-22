aeca5b2e4a003968c8128ac976652d965f657056
wangxiaofei <wangxiaofei2004@163.com>
2021-04-22 14:50:56 +0800

    Create TopLogin.tsx

---
+++ demos/demo20/src/features/toplogin/TopLogin.tsx
@@ -0,0 +1,37 @@
+import './i18n'
+// the hook
+import { useTranslation } from 'react-i18next';
+
+
+function TopLogin() {
+
+
+ const theme = useAppSelector(selectTheme);
+ const login = useAppSelector(selectLogin);
+ const dispatch = useAppDispatch();
+
+  return (
+
+
+{if(!login){
+                
+                <Nav.Link as button href="#/login">{t('Login')}</Nav.Link>
+                
+                }else {
+                
+                <Dropdown as={ButtonGroup}>
+                   <Image src="holder.js/171x180" roundedCircle />
+                  <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+                
+                }}
+
+);
+
+
+}
