f7a23a1637e8fed4409a9c5e800b0201e3d7782e
wangxiaofei <wangxiaofei2004@163.com>
2021-04-22 16:09:07 +0800

    Update and rename ShowFrom.tsx to LogoutFrom.tsx

---
+++ demos/demo20/src/features/login/LogoutFrom.tsx
@@ -0,0 +1,31 @@
+import { Alert } from 'react-bootstrap';
+// the hook
+import { useTranslation } from 'react-i18next';
+
+export function LogoutFrom() {
+
+const { t, i18n } = useTranslation();
+      
+const login = useAppSelector(selectLogin);
+const theme = useAppSelector(selectTheme);
+const dispatch = useAppDispatch();
+
+return (
+
+     <Form>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Name" plaintext readOnly />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+);
+
+}
