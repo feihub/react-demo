a491da1fe32fd0619eea91415b4e1426348a83d6
wangxiaofei <wangxiaofei2004@163.com>
2021-04-22 16:07:08 +0800

    Update LoginFrom.tsx

--- demos/demo20/src/features/login/LoginFrom.tsx
+++ demos/demo20/src/features/login/LoginFrom.tsx
@@ -1,1 +1,35 @@
+import { Alert } from 'react-bootstrap';
+// the hook
+import { useTranslation } from 'react-i18next';
 
+export function LoginFrom() {
+
+const { t, i18n } = useTranslation();
+      
+const login = useAppSelector(selectLogin);
+const theme = useAppSelector(selectTheme);
+const dispatch = useAppDispatch();
+
+return (
+
+      <Form>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Enter name" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+                  
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+);
+
+}
