48d304e68557b6480a2e607764b31dde13f5e14b
wangxiaofei <wangxiaofei2004@163.com>
2021-04-22 15:59:36 +0800

    Update Center.tsx

--- demos/demo20/src/features/home/Center.tsx
+++ demos/demo20/src/features/home/Center.tsx
@@ -1,1 +1,30 @@
+import { Alert } from 'react-bootstrap';
+// the hook
+import { useTranslation } from 'react-i18next';
 
+export function Center() {
+
+const { t, i18n } = useTranslation();
+    
+const theme = useAppSelector(selectTheme);
+const dispatch = useAppDispatch();
+
+return (
+
+<main>
+
+            <Alert id='item_1'>this is item_1 </Alert>
+
+            <Alert id='item_2'>
+                
+                this is item_2 
+
+                <Alert id='item_2_1'>this is item_2_1 </Alert>
+
+            </Alert>
+
+            <Alert id='item2_1'>this is item2_1 </Alert>
+
+        </main>
+);
+}
