b6a786159177ca601a20ed544b2625e1375f9887
wangxiaofei <wangxiaofei2004@163.com>
2021-04-22 14:52:36 +0800

    Update Top.tsx

--- demos/demo20/src/features/top/Top.tsx
+++ demos/demo20/src/features/top/Top.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next';
 
 function Top() {
 
+   const { t, i18n } = useTranslation();
 
  const theme = useAppSelector(selectTheme);
   const login = useAppSelector(selectLogin);
@@ -46,27 +47,7 @@ function Top() {
                   <NavDropdown.Item eventKey="2">中文</NavDropdown.Item>
                 </NavDropdown>
               </Nav></Nav.Item>
-            <Nav.Item className="me-5">
-              
-              if(!login){
-                
-                <Nav.Link as button href="#/login">{t('Login')}</Nav.Link>
-                
-                }else {
-                
-                <Dropdown as={ButtonGroup}>
-                   <Image src="holder.js/171x180" roundedCircle />
-                  <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
-                  <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-                
-                }
-              
-          </Nav.Item>
+            <Nav.Item className="me-5"><TopLogin /></Nav.Item>
           </Nav>
       </Navbar >
 
