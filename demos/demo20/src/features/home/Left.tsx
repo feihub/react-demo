dab16e78cc49bd71fe3b76e38f1e8158891ba8e7
wangxiaofei <wangxiaofei2004@163.com>
2021-04-22 15:58:44 +0800

    Update Left.tsx

--- demos/demo20/src/features/home/Left.tsx
+++ demos/demo20/src/features/home/Left.tsx
@@ -1,1 +1,19 @@
+import { Alert } from 'react-bootstrap';
+// the hook
+import { useTranslation } from 'react-i18next';
 
+export function Left() {
+
+const { t, i18n } = useTranslation();
+    
+const theme = useAppSelector(selectTheme);
+const dispatch = useAppDispatch();
+
+return (
+
+<Nav defaultActiveKey="#/home/item" className="flex-column">
+                <Nav.Link href="#/home/item">Active</Nav.Link>
+                <Nav.Link href="#/home/item2">Link</Nav.Link>
+            </Nav>
+);
+}
