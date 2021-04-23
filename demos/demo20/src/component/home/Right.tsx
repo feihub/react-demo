035cafffd16c8aa50ba36d825574431fcf20d200
wangxiaofei <wangxiaofei2004@163.com>
2021-04-23 10:40:18 +0800

    Update Right.tsx

--- demos/demo20/src/component/home/Right.tsx
+++ demos/demo20/src/component/home/Right.tsx
@@ -19,20 +19,20 @@ export function Right() {
 
     <Container className = 'mt-3' fluid>
         <Switch>
-          <Route path="/home/item">
-            <ListGroup defaultActiveKey="#item_1">
-              <ListGroup.Item className = 'border-0 py-0'><Nav.Link href="#/home/item/1">{t('Component')}</Nav.Link></ListGroup.Item>
+          <Route path="/home/item1">
+            <ListGroup defaultActiveKey="#item11">
+              <ListGroup.Item className = 'border-0 py-0'><Nav.Link href="#item11">{t('Component')}</Nav.Link></ListGroup.Item>
               <ListGroup.Item className = 'border-0 py-0'>
-                <Nav.Link href="#/home/item/2">{t('Lifecycle')}</Nav.Link>
+                <Nav.Link href="#item12">{t('Lifecycle')}</Nav.Link>
                 <ListGroup >
-                  <ListGroup.Item  className = 'border-0 py-0'><Nav.Link href="#/home/item/2/2">{t('State')}</Nav.Link></ListGroup.Item>
+                  <ListGroup.Item  className = 'border-0 py-0'><Nav.Link href="#item13">{t('State')}</Nav.Link></ListGroup.Item>
                 </ListGroup>
               </ListGroup.Item>
             </ListGroup>
           </Route>
           <Route path="/home/item2">
-            <ListGroup defaultActiveKey="#/home/item2">
-              <ListGroup.Item className = 'border-0 py-0'><Nav.Link href="#/home/item2/1">{t('Example1')}</Nav.Link></ListGroup.Item></ListGroup>
+            <ListGroup defaultActiveKey="#item21">
+              <ListGroup.Item className = 'border-0 py-0'><Nav.Link href="#item21">{t('Example1')}</Nav.Link></ListGroup.Item></ListGroup>
           </Route>
         </Switch>
     </Container>
