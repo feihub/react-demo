e22b6758a825b4f7d30a12b7f971a74833443237
wangxiaofei <wangxiaofei2004@163.com>
2021-05-06 15:51:14 +0800

    Update App.js

--- demos/demo17/src/App.js
+++ demos/demo17/src/App.js
@@ -1,1 +1,14 @@
+import './i18n'
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <HookApp/>
+      <HOCApp/>
+      <App/>
+    </div>
+  );
+}
+
+export default App;
