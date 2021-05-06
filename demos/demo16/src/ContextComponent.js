f01428c5352365acca7ccbfec39aa3cc3b588ed3
wangxiaofei <wangxiaofei2004@163.com>
2021-05-06 16:05:57 +0800

    Update and rename ContextApp.js to ContextComponent.js

--- demos/demo16/src/ContextComponent.js
+++ demos/demo16/src/ContextComponent.js
@@ -1,7 +1,7 @@
 // Context 可以让我们无须明确地传遍每一个组件，就能将值深入传递进组件树。
 // 为当前的 theme 创建一个 context（“light”为默认值）。
 const ThemeContext = React.createContext('light');
-class ContextApp extends React.Component {
+class ContextComponent extends React.Component {
   render() {
     // 使用一个 Provider 来将当前的 theme 传递给以下的组件树。
     // 无论多深，任何组件都能读取这个值。
