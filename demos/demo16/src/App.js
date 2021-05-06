48c5a58c19e20bafad182d69d8b6f1711f1c841d
wangxiaofei <wangxiaofei2004@163.com>
2021-05-06 16:09:25 +0800

    Create App.js

---
+++ demos/demo16/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Grandfather from './features/test/Grandfather';
+import './App.css';
+
+function App() {
+  return (
+     <NormalComponent />
+     <ContextComponent />
+     <ReduxComponent />
+  );
+}
+
+export default App;
