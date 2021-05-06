bc647d4c75027b752b427bd6c306cf119a0c76a8
wangxiaofei <wangxiaofei2004@163.com>
2021-05-06 15:31:30 +0800

    Update and rename App.js to ReduxApp.js

--- demos/demo16/src/ReduxApp.js
+++ demos/demo16/src/ReduxApp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Grandfather from './features/test/Grandfather';
 import './App.css';
 
-function App() {
+function Redux() {
   return (
     <div className="App">
       <Grandfather />
