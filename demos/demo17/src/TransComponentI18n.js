03bdde3daac7f194adf799279dc0643ade5f0ae8
wangxiaofei <wangxiaofei2004@163.com>
2021-05-08 10:20:42 +0800

    update demo17

--- demos/demo17/src/TransComponentI18n.js
+++ demos/demo17/src/TransComponentI18n.js
@@ -3,7 +3,13 @@ import { Trans, useTranslation } from 'react-i18next'
 
 //Using the Trans component
 
-export default function TransComponentI18n () {
+export default function TransComponentI18n() {
   const { t } = useTranslation();
-  return <Trans t={t} >Language</Trans>;
+  //return <Trans t={t} >Language</Trans>;
+  const count = 888;
+  const name = "Panda";
+
+  return <Trans i18nKey="userMessagesUnread" count={count} name={name} t={t}>
+            Hello <strong title={t('Language')}>{{ name }}</strong>, you have {{ count }} unread message.<small>测试</small>
+          </Trans>;
 }
