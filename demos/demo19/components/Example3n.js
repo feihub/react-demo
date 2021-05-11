014abe3a9958e4a46958058aa979cb15661d1afa
wangxiaofei <wangxiaofei2004@163.com>
2021-05-11 11:17:42 +0800

    Create Example3n.js

---
+++ demos/demo19/components/Example3n.js
@@ -0,0 +1,15 @@
+const FancyButton = React.forwardRef((props, ref) => (
+    <button ref={ref} className="FancyButton">
+      {props.children}
+    </button>
+  ));
+  
+  // 你可以直接获取 DOM button 的 ref：
+  const ref = React.createRef();
+  <FancyButton ref={ref}>Click me!</FancyButton>;
+
+const Example3n = () => {
+    return <FancyButton/>;
+}
+
+export default Example3n;
