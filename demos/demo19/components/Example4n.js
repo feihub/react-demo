12152b338647800ccf4ab1d3c5ac7c458b4785ce
wangxiaofei <wangxiaofei2004@163.com>
2021-05-11 13:42:58 +0800

    Update Example4n.js

--- demos/demo19/components/Example4n.js
+++ demos/demo19/components/Example4n.js
@@ -21,6 +21,24 @@ class Table extends React.Component {
   }
 }
 
+
+
+//使用显式 <React.Fragment> 语法声明的片段可能具有 key。一个使用场景是将一个集合映射到一个 Fragments 数组
+
+function Glossary(props) {
+  return (
+    <dl>
+      {props.items.map(item => (
+        // 没有`key`，React 会发出一个关键警告
+        <React.Fragment key={item.id}>
+          <dt>{item.term}</dt>
+          <dd>{item.description}</dd>
+        </React.Fragment>
+      ))}
+    </dl>
+  );
+}
+
 const Example4n = () => {
     return <Table/>;
 }
