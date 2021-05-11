916afbaf1db13f672ef655fa1dcc6d4ee668153d
wangxiaofei <wangxiaofei2004@163.com>
2021-05-11 13:37:23 +0800

    Update App.js

--- demos/demo19/App.js
+++ demos/demo19/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Title from "./components/Title";
-import ExampleCount from "./components/ExampleCount";
-import ExampleCount2n from "./components/ExampleCount2n";
-import ExampleCount3n from "./components/ExampleCount3n";
-import ExampleCount4n from "./components/ExampleCount4n";
-import ExampleCount5n from "./components/ExampleCount5n";
-import ExampleCount6n from "./components/ExampleCount6n";
-import ExampleCount7n from "./components/ExampleCount7n";
-import ExampleCount8n from "./components/ExampleCount8n";
+import Example from "./components/Example";
+import Example2n from "./components/Example2n";
+import Example3n from "./components/Example3n";
+import Example4n from "./components/Example4n";
+import Example5n from "./components/Example5n";
+import Example6n from "./components/Example6n";
+import Example7n from "./components/Example7n";
+import Example8n from "./components/Example8n";
 
 function App() {
 return (
@@ -16,11 +16,11 @@ return (
     <div className="container">
         <div className="row">
             <Title titleText="Context" />
-            <ExampleCount />
+            <Example />
         </div>
         <div className="row">
             <Title titleText="错误边界" />
-            <ExampleCount2n />
+            <Example2n />
         </div>
         <div className="row">
             <hr>
@@ -28,7 +28,7 @@ return (
         </div>
         <div className="row">
             <Title titleText="Refs 转发" />
-            <ExampleCount3n />
+            <Example3n />
         </div>
         <div className="row">
             <hr>
@@ -36,7 +36,7 @@ return (
         </div>
         <div className="row">
             <Title titleText="Fragments" />
-            <ExampleCount4n />
+            <Example4n />
         </div>
         <div className="row">
             <hr>
@@ -44,11 +44,11 @@ return (
         </div>
         <div className="row">
             <Title titleText="Refs & DOM" />
-            <ExampleCount5n />
+            <Example5n />
         </div>
         <div className="row">
             <Title titleText="静态类型检查" />
-            <ExampleCount6n />
+            <Example6n />
         </div>
         <div className="row">
             <hr>
@@ -56,11 +56,11 @@ return (
         </div>
         <div className="row">
             <Title titleText="严格模式" />
-            <ExampleCount7n />
+            <Example7n />
         </div>
         <div className="row">
             <Title titleText="API Reference" />
-            <ExampleCount8n />
+            <Example8n />
         </div>
     </div>
 </div>
