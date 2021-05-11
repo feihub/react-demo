a9b275e7465679948baa24ddf387a38d41698818
wangxiaofei <wangxiaofei2004@163.com>
2021-05-11 14:20:05 +0800

    Update and rename ExampleCount.js to Example.js

--- demos/demo11/src/components/Example.js
+++ demos/demo11/src/components/Example.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-function ExampleCount() {
+function Example() {
   const [count, setCount] = useState(0); // useState 就是一个Hook，这里使用解构，最好使用这种格式解构赋值
   const [fruit, setFruit] = useState("banana");
   const [todos, setTodos] = useState([{ text: "Learn Hooks" }]); // 可以声明多个
@@ -21,4 +21,4 @@ function ExampleCount() {
  * useState
  * 返回一个变量和一个改变变量值的函数
  */
-export default ExampleCount;
+export default Example;
