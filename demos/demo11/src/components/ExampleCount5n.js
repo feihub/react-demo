c6863a9ab2e31740ac05fecc77f7032f221ba25b
wangxiaofei <wangxiaofei2004@163.com>
2021-05-11 10:15:02 +0800

    Create ExampleCount5n.js

---
+++ demos/demo11/src/components/ExampleCount5n.js
@@ -0,0 +1,19 @@
+function TextInputWithFocusButton() {
+    const inputEl = useRef(null);
+    const onButtonClick = () => {
+        // `current` 指向已挂载到 DOM 上的文本输入元素
+        inputEl.current.focus();
+    };
+    return (
+        <>
+            <input ref={inputEl} type="text" />
+            <button onClick={onButtonClick}>Focus the input</button>
+        </>
+    );
+}
+
+const ExampleCount5n = () => {
+    return <TextInputWithFocusButton/>;
+}
+
+export default ExampleCount5n;
